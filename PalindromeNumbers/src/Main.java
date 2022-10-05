0e948621fe37668bb73b58159c51c265d02310f4
Hakan Boranbay <boranbayhakan@gmail.com>
2022-10-06 00:01:35 +0300

    PalindromeNumbers practise added.
    A method to find out if a number is a palindrome number.

---
+++ PalindromeNumbers/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindromeNumber(1));
+        System.out.println(isPalindromeNumber(4));
+        System.out.println(isPalindromeNumber(8));
+        System.out.println(isPalindromeNumber(99));
+        System.out.println(isPalindromeNumber(101));
+        System.out.println(isPalindromeNumber(363));
+        System.out.println(isPalindromeNumber(4004));
+        System.out.println(isPalindromeNumber(9889));
+    }
+
+    public static boolean isPalindromeNumber(int number) {
+
+        int newNumber = 0;
+        int temp = number;
+
+        while(temp > 0) {
+            newNumber = (newNumber * 10) + (temp % 10);
+            temp /= 10;
+        }
+        return number == newNumber;
+    }
+}
