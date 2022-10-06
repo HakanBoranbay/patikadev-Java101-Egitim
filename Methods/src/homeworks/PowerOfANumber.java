aa8d46800858ea0c66f0adb41b997d5cf24a8506
Hakan Boranbay <boranbayhakan@gmail.com>
2022-10-06 16:30:30 +0300

    PowerOfANumber committed.
    A program to calculate the desired power of a number.

--- Methods/src/homeworks/PowerOfANumber.java
+++ Methods/src/homeworks/PowerOfANumber.java
@@ -9,31 +9,18 @@ public class PowerOfANumber {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter base number: ");
         int base = scanner.nextInt();
-        System.out.println("Enter exponent: ");
+        System.out.println("Enter a positive exponent: ");
         int exp = scanner.nextInt();
 
         System.out.println("Result: " + power(base,exp));
 
     }
 
-    public static double power(int number, int exponent) {
+    public static int power(int number, int exponent) {
 
-        if (exponent == 0) return number;
+        if (exponent == 0) return 1;
+        if (exponent == 1) return number;
 
-        double power = 1;
-
-        if (exponent == 0) {
-            return 1;
-        } else if (exponent > 0) {
-            for (int i = 0; i < exponent; i++) {
-                power *= number;
-            }
-        } else {
-            for (int i = 0; i > exponent; i--) {
-                power /= number;
-            }
-        }
-
-        return power;
+        return power(number, --exponent) * number;
     }
 }
