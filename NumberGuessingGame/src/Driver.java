4d5c398cc171ae32f9b7ec7a4c4c1c630094d9c7
Hakan Boranbay <boranbayhakan@gmail.com>
2022-11-15 20:39:26 +0300

    NumberGuessingGame project comitted.
    A number guessing game is committed. A user can guess a target number between 0 and 100 and has 5 tries.

---
+++ NumberGuessingGame/src/Driver.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		try (Scanner scanner = new Scanner(System.in)) {
+			Random random = new Random();
+			int targetNumber = random.nextInt(100);
+			int right = 5;
+			
+			while (right > 0) {
+				System.out.print("Please enter your guess (tries left = " + right + "): ");
+				int guessedNumber = scanner.nextInt();
+				
+				if (guessedNumber < 0 || guessedNumber > 100) {
+					System.out.println("Please enter a number between 0 and 100.");
+					continue;
+				}
+				
+				if (guessedNumber == targetNumber) {
+					System.out.println("Congratulations! You Won!");
+				} else {
+					right--;
+					if (guessedNumber < targetNumber) {
+						System.out.println("Higher!");					
+					} else {
+						System.out.println("Lower!");
+					}
+				}
+				
+				if (right == 0) {
+					System.out.println("Game Over!");
+					System.out.println("The number was " + targetNumber);
+				}
+			}
+		}
+	}
+}
